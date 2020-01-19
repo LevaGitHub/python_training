ef0c32a6a16d01785be5ffee991f998e01d5d3bb
LevaGitHub <lvov_oleg@list.ru>
2020-01-19 21:42:08 +0300

    Вспомогательные методы вынесены в класс Application

--- test_add_group.py
+++ test_add_group.py
@@ -7,12 +7,12 @@ import string
 import random
 from group import Group
 from person import Person
+from application import Application
 
 days = [i for i in range(1, 32)]
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
           "December"]
 years = [i for i in range(1970, 2000)]
-
 
 def generate_chars_sequence(size, seq):
     return ''.join(random.choice(seq) for _ in range(size))
@@ -20,63 +20,19 @@ def generate_chars_sequence(size, seq):
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.app = Application()
 
     def test_add_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="name", header="group header", footer="group footer"))
-        self.return_to_groups_page()
-        self.logout()
+        self.app.login(username="admin", password="secret")
+        self.app.create_group(Group(name="name", header="group header", footer="group footer"))
+        self.app.return_to_groups_page()
+        self.app.logout()
 
     def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.return_to_groups_page()
-        self.logout()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("https://localhost/addressbook/")
+        self.app.login(username="admin", password="secret")
+        self.app.create_group(Group(name="", header="", footer=""))
+        self.app.return_to_groups_page()
+        self.app.logout()
 
     def is_element_present(self, how, what):
         try:
@@ -93,11 +49,11 @@ class TestAddGroup(unittest.TestCase):
         return True
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
     def test_add_person(self):
-        self.login(username="admin", password="secret")
-        self.open_person()
+        self.app.login(username="admin", password="secret")
+        self.app.open_person()
         pers = Person(firstname=generate_chars_sequence(20, string.ascii_letters),
                       middlename=generate_chars_sequence(20, string.ascii_letters),
                       lastname=generate_chars_sequence(20, string.ascii_letters),
@@ -122,90 +78,9 @@ class TestAddGroup(unittest.TestCase):
                       address2=generate_chars_sequence(20, string.ascii_letters),
                       phone2=generate_chars_sequence(11, string.digits),
                       notes=generate_chars_sequence(50, string.ascii_letters))
-        self.input_person_data(pers)
-        self.return_to_home_page()
-        self.logout()
-
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def input_person_data(self, pers):
-        wd = self.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(pers.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(pers.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(pers.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(pers.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(pers.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(pers.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(pers.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(pers.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(pers.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(pers.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(pers.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(pers.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(pers.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(pers.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(pers.homepage)
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bday").send_keys("%s" % pers.bday)
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("bmonth").send_keys("%s" % pers.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(pers.byear)
-        wd.find_element_by_name("aday").click()
-        wd.find_element_by_name("aday").click()
-        wd.find_element_by_name("aday").send_keys("%s" % pers.aday)
-        wd.find_element_by_name("amonth").click()
-        wd.find_element_by_name("amonth").send_keys("%s" % pers.amonth)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(pers.ayear)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(pers.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(pers.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(pers.notes)
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def open_person(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
+        self.app.input_person_data(pers)
+        self.app.return_to_home_page()
+        self.app.logout()
 
 if __name__ == "__main__":
     unittest.main()
