aee7c79886b4da7a1958bc4c290f07247b88385e
LevaGitHub <lvov_oleg@list.ru>
2020-01-19 22:56:45 +0300

    Учтены замечания задания 3
    Тест создания контакта вынесен в отдельный файл

---
+++ test_add_person.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import string
+import random
+from person import Person
+from application import Application
+
+days = [i for i in range(1, 32)]
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
+          "December"]
+years = [i for i in range(1970, 2000)]
+
+
+def generate_chars_sequence(size, seq):
+    return ''.join(random.choice(seq) for _ in range(size))
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_person(app):
+    app.login(username="admin", password="secret")
+    app.open_person_page()
+    pers = Person(firstname=generate_chars_sequence(20, string.ascii_letters),
+                  middlename=generate_chars_sequence(20, string.ascii_letters),
+                  lastname=generate_chars_sequence(20, string.ascii_letters),
+                  nickname=generate_chars_sequence(20, string.ascii_letters),
+                  title=generate_chars_sequence(20, string.ascii_letters),
+                  company=generate_chars_sequence(20, string.ascii_letters),
+                  address=generate_chars_sequence(20, string.ascii_letters),
+                  home=generate_chars_sequence(2, string.digits),
+                  mobile=generate_chars_sequence(11, string.digits),
+                  work=generate_chars_sequence(20, string.ascii_letters),
+                  fax=generate_chars_sequence(11, string.digits),
+                  email=generate_chars_sequence(20, string.ascii_letters),
+                  email2=generate_chars_sequence(20, string.ascii_letters),
+                  email3=generate_chars_sequence(20, string.ascii_letters),
+                  homepage=generate_chars_sequence(20, string.ascii_letters),
+                  bday=random.choice(days),
+                  bmonth=random.choice(months),
+                  byear=random.choice(years),
+                  aday=random.choice(days),
+                  amonth=random.choice(months),
+                  ayear=random.choice(years),
+                  address2=generate_chars_sequence(20, string.ascii_letters),
+                  phone2=generate_chars_sequence(11, string.digits),
+                  notes=generate_chars_sequence(50, string.ascii_letters))
+    app.input_person_data(pers)
+    app.return_to_home_page()
+    app.logout()
