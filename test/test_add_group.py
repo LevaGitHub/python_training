8497f1c6b3aa2430f35e3e860a433acbee656e06
LevaGitHub <lvov_oleg@list.ru>
2020-01-20 23:06:13 +0300

    Выделен класс для работы с группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -15,13 +15,13 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="name", header="group header", footer="group footer"))
-    app.return_to_groups_page()
+    app.group.create(Group(name="name", header="group header", footer="group footer"))
+    app.group.return_to_groups_page()
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
-    app.return_to_groups_page()
+    app.group.create(Group(name="", header="", footer=""))
+    app.group.return_to_groups_page()
     app.session.logout()
