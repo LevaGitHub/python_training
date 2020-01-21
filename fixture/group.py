d7a4c0de23e7ad28ca555c8512acaee75cd68e14
LevaGitHub <lvov_oleg@list.ru>
2020-01-21 22:40:16 +0300

    Добавлен тест редактирования первой группы

--- fixture/group.py
+++ fixture/group.py
@@ -9,14 +9,7 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.input_group_data(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -24,6 +17,16 @@ class GroupHelper:
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
+
+    def edit_first_group(self, edited_group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        self.input_group_data(edited_group)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -33,6 +36,17 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def input_group_data(self, group):
+        wd = self.app.wd
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
 
     def return_to_groups_page(self):
         wd = self.app.wd
---
+++ test/test_edit_group.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from model.group import Group
+
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="edited_name", header="edited_group_header", footer="edited_group_footer"))
+    app.session.logout()
