86519d0257c4278429cddfbe223d66b3ad3d2d0e
LevaGitHub <lvov_oleg@list.ru>
2020-02-04 23:23:19 +0300

    Добавлена загрузка списка групп
    Добавлено получение списка групп до теста и сравнение через assert

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -10,4 +10,7 @@ def checking_preconditions_before_delete(app):
 
 def test_delete_first_group(app):
     checking_preconditions_before_delete(app)
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -10,14 +10,23 @@ def checking_preconditions_before_edit(app):
 
 def test_edit_first_group_name(app):
     checking_preconditions_before_edit(app)
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="edited_name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_header(app):
     checking_preconditions_before_edit(app)
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="edited_group_header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_footer(app):
     checking_preconditions_before_edit(app)
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(footer="edited_group_footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
