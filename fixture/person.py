2e1a901a19950a0fc54aab2427a370a75e23d395
LevaGitHub <lvov_oleg@list.ru>
2020-02-13 22:41:15 +0300

    Реализованы обратные проверки телефонов
    Обратные проверки для телефонов на главной странице и на форме редактирования контактов

--- fixture/person.py
+++ fixture/person.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from model.person import Person
+import re
 
 
 def get_edit_button_name(index):
@@ -8,6 +9,13 @@ def get_edit_button_name(index):
         return "// img[ @ alt = 'Edit']"
     else:
         return "(// img[ @ alt = 'Edit'])[{}]".format(index+1)
+
+
+def get_view_button_name(index):
+    if index == 0:
+        return "// img[ @ alt = 'Details']"
+    else:
+        return "(// img[ @ alt = 'Details'])[{}]".format(index + 1)
 
 
 class PersonHelper:
@@ -74,12 +82,20 @@ class PersonHelper:
 
     def edit_person_by_index(self, index, edited_person):
         wd = self.app.wd
-        self.open_all_person_page()
-        self.select_person_by_index(index)
+        self.open_person_to_edit_by_index(index)
         self.input_person_data(edited_person)
         wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
         self.open_all_person_page()
         self.person_cache = None
+
+    def open_person_to_edit_by_index(self, index):
+        self.open_all_person_page()
+        self.select_person_by_index(index)
+
+    def open_person_to_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_all_person_page()
+        wd.find_element_by_xpath(get_view_button_name(index)).click()
 
     def return_to_home_page(self):
         wd = self.app.wd
@@ -106,6 +122,38 @@ class PersonHelper:
                 lastname = column_lastname.text
                 column_firstname = columns[2]
                 firstname = column_firstname.text
-                self.person_cache.append(Person(firstname=firstname, lastname=lastname, person_id=person_id))
+                all_phones = columns[5].text
+                self.person_cache.append(Person(firstname=firstname, lastname=lastname, person_id=person_id,
+                                                all_phones_from_homepage=all_phones))
         return self.person_cache
 
+    def get_person_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_person_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home = wd.find_element_by_name("home").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        work = wd.find_element_by_name("work").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Person(firstname=firstname,
+                      lastname=lastname,
+                      person_id=id,
+                      home=home,
+                      mobile=mobile,
+                      work=work,
+                      phone2=phone2)
+
+    def get_person_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_person_to_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home = re.search("H: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        work = re.search("W: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Person(home=home,
+                      mobile=mobile,
+                      work=work,
+                      phone2=phone2)
